82d29665249f8b06ce7e6e9c1f4980a2109fa7ac
Son Truong <sont85@gmail.com>
2015-07-05 12:45:17 -0700

    changed heroku env variable

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 var MD5 = require("MD5");
 var slug = require("slug");
 
-mongoose.connect(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGOLAB_URI);
 // mongoose.connect('mongodb:/localhost/test');
 
 // var User = mongoose.model("users", { name: String });
